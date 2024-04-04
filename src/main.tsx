201727252c2cda8052248721f9113027c72900a0
Denis Oliver Kropp <dok@directfb1.org>
2024-04-04 13:18:29 +0200

    initial

---
+++ src/main.tsx
@@ -0,0 +1,34 @@
+import * as THREE from 'three'
+import { createRoot } from 'react-dom/client'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { getProject } from '@theatre/core'
+import studio from '@theatre/studio'
+///import extension from '@theatre/r3f/dist/extension'
+
+studio.initialize()
+///studio.extend(extension)
+
+// our Theatre.js project sheet, we'll use this later
+const demoSheet = getProject('Demo Project').sheet('Demo Sheet')
+
+const App = () => {
+    return (
+        <Canvas
+            style={{ height: '100vh', margin: 0 }}
+            camera={{
+                position: [5, 5, -5],
+                fov: 75,
+            }}
+        >
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <mesh>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshStandardMaterial color="orange" />
+            </mesh>
+        </Canvas>
+    )
+}
+
+createRoot(document.getElementById('root')!).render(<App />)
